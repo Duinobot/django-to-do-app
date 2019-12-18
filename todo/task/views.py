71eceff5fbe2749b66a2bd8d715bfe46a469d3be
Superdan <danmo2060@gmail.com>
2019-12-19 09:44:25 +1100

    before form

---
+++ todo/task/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import *
+
+
+# Create your views here.
+def index(request):
+    task = Task.objects.all()
+
+    context = {
+        "task" : task
+    }
+    return render(request, 'task/list.html', context)
