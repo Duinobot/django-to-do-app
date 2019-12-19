3afcd8b937533dd223a55ea71932e77876e67019
Superdan <danmo2060@gmail.com>
2019-12-20 09:57:40 +1100

    update update list function

--- todo/task/views.py
+++ todo/task/views.py
@@ -1,14 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import *
+from .forms import TaskForm
 
 
 # Create your views here.
 def index(request):
     task = Task.objects.all()
 
+    form = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid:
+            form.save()
+        return redirect('/')
+
     context = {
-        "task" : task
+        "task" : task,
+        "form" : form
     }
     return render(request, 'task/list.html', context)
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+    itemForm = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        itemForm = TaskForm(request.POST, instance=task)
+        if itemForm.is_valid:
+            itemForm.save()
+            return redirect('/')
+    
+    context = {
+        'task' : task,
+        'itemForm' : itemForm
+    }
+    return render(request, 'task/update_task.html', context)
